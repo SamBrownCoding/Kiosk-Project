ccac126dd085f0b1cd0818f419eccb03cb82d454
SamBrownCoding <166549412+SamBrownCoding@users.noreply.github.com>
2024-07-25 15:10:20 -0500

    Update

--- obj/Debug/net8.0/Kiosk-Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Kiosk-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kiosk-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30ca87a416e40e999e764149d4cd98f58ac90697")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kiosk-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kiosk-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
